dd47643d03455ac593790d6fb03a293ce0e44c4e
Your Name <carlosvanzego96@gmail.com>
2024-02-11 04:05:16 -0500

    [message]

--- Tuples/script.ts
+++ Tuples/script.ts
@@ -10,3 +10,13 @@ console.log(tuple[1]) // Output: 14
 
 // Tuple type inference
 let anotherTuple = ["world", 27]; // TypeScript infers the type as [string, number]
+
+
+// Function returning a tuple
+function getUsernameAndAge(): [string, number] {
+    return["Carlos", 27]
+}
+
+let [username, age] = getUsernameAndAge(); // Destructuring the Tuple
+console.log(username); //Output: "Carlos"
+console.log(age); //Output: 27
