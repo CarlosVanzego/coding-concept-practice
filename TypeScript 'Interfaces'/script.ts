9d5447de85ee6ddb8688089242085b73920158e8
Your Name <carlosvanzego96@gmail.com>
2024-02-11 01:00:27 -0500

    [message]

---
+++ TypeScript 'Interfaces'/script.ts
@@ -0,0 +1,14 @@
+interface UserInter {
+  name: string;
+  age: number;
+}
+
+interface Person {
+  name: string;
+  age: 27;
+}
+
+const carlos: Person = {
+  name: "Carlos",
+  age: 27,
+}
---
+++ TypeScript 'Types'/script.ts
@@ -0,0 +1,15 @@
+type UserType = {
+  name: string
+  age: number
+}
+
+type Person = {
+  name: string;
+  age: number;
+}
+
+const carlos: Person = {
+  name: "Carlos",
+  age: 27
+}
+
