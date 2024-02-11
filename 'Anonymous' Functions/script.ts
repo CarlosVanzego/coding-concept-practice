43e0d2ef956fdf4b0f5076800af156045f985a9c
Your Name <carlosvanzego96@gmail.com>
2024-02-11 02:17:04 -0500

    [message]

---
+++ 'Anonymous' Functions/script.ts
@@ -0,0 +1,12 @@
+let greeting = function() {
+    console.log("Hello TypeScript!");
+};
+
+greeting(); //Output: Hello TypeScript!
+
+let Sum = function(x: number, y: number) : number 
+{
+    return x + y
+}
+
+Sum(2,3); // retruns 5
---
+++ 'Named' Functions /scripts.ts
@@ -0,0 +1,11 @@
+function display() {
+    console.log("Hello TypeScript!");
+}
+
+display(); //Output: Hello TypeScript
+
+function Sum(x: number, y: number) : number {
+    return x + y
+}
+
+Sum(2, 3); // returns 5
